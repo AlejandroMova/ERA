b49db975eacb119c74c4c344a815052e780111dd
AlejandroMova <122325572+AlejandroMova@users.noreply.github.com>
2023-05-30 17:25:02 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,6 @@ const path = require('path');
 const app = express(); 
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
-app.listen(process.env.PORT, (req, res) => {
-    console.log("Listening"); 
-})
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
