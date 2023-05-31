502fa3a62226c2a9bf053b4e61784c131105473d
AlejandroMova <alejandro@movawebservices.com>
2023-05-30 18:35:01 -0600

    Build folder

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const port = process.env.PORT || 3000; 
 // express app
 const app = express(); 
-app.use(express.static(path.resolve(__dirname, './frontend/build')));
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
   });
 
 app.listen(port, () => console.log('Listening')); 
