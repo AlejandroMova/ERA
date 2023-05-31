8db2a60806a670e9511a75f9a6ad9bbde1e405ed
AlejandroMova <alejandro@movawebservices.com>
2023-05-30 18:18:44 -0600

    Second Version

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config(); 
 const path = require('path'); 
 
+const port = process.env.PORT || 3000; 
 // express app
 const app = express(); 
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
@@ -10,3 +11,5 @@ app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
   });
+
+app.listen(port, () => console.log('Listening')); 
